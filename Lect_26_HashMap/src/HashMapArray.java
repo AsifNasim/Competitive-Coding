a7ef1b03f7bbbdcc2576a586437f2a5fabade1c3
AsifNasim <asifnasimofficial@gmail.com>
2020-04-15 14:09:40 +0530

    HashMap using array has been added.

---
+++ Lect_26_HashMap/src/HashMapArray.java
@@ -0,0 +1,33 @@
+public class HashMapArray {
+    public class Entity{
+        String key;
+        String value;
+
+        public Entity(String key, String value) {
+            this.key = key;
+            this.value = value;
+
+        }
+    }
+    Entity[] entities = new Entity[100];
+
+    public void push(String key, String value){
+        int hashcode = Math.abs(key.hashCode()%entities.length);
+        entities[hashcode] = new Entity(key, value);
+    }
+
+    public String get(String key){
+        int hashcode = Math.abs(key.hashCode()%entities.length);
+        if(entities[hashcode]!= null && entities[hashcode].key.equals(key)){
+            return entities[hashcode].value;
+        }
+        return null;
+    }
+
+    public void remove(String key){
+        int hashcode = Math.abs(key.hashCode()%entities.length);
+        if(entities[hashcode]!= null && entities[hashcode].key.equals(key)){
+            entities[hashcode]= null;
+        }
+    }
+}
